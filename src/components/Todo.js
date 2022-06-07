8675c2a83a4161b3ae388df1d98ee55b7a9b3eea
hoangnv <viethoang.ma1009@gmail.com>
2022-06-07 15:40:52 +0000

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -42,13 +42,27 @@ function Todo() {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
 
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  const handleFilterChange = value => setFilter(value);
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input addWork={handleAdding} />
-      {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         // <label className="panel-block">
         //     <input type="checkbox" />
         //     {item.text}
@@ -59,7 +73,7 @@ function Todo() {
           onCheck={handleCheck} />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
